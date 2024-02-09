897b7424e14fd45de18141438f37e95ba6de7845
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2024-02-09 10:03:30 +0100

    test: table-test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,28 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 6, b: 2, action: Action.Subtract, expected: 4 },
+  { a: 12, b: 13, action: Action.Subtract, expected: -1 },
+  { a: 1, b: 1, action: Action.Subtract, expected: 0 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 6, b: 7, action: Action.Multiply, expected: 42 },
+  { a: -5, b: 6, action: Action.Multiply, expected: -30 },
+  { a: 10, b: 5, action: Action.Divide, expected: 2 },
+  { a: 3, b: 3, action: Action.Divide, expected: 1 },
+  { a: -30, b: -6, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 5, b: 3, action: Action.Exponentiate, expected: 125 },
+  { a: 9, b: 2, action: Action.Exponentiate, expected: 81 },
+  { a: '9', b: 2, action: Action.Exponentiate, expected: null },
+  { a: 5, b: '2', action: Action.Exponentiate, expected: null },
+  { a: 7, b: 6, action: '%', expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('should return %p', ({ a, b, action, expected }) => {
+    expect(simpleCalculator({ a, b, action })).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
