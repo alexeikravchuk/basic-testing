10dce8704301a4cfa2acc5bd2a3fb04fb9a0e82f
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2024-02-10 12:19:44 +0100

    refactor: error-handling used toThrowError

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -16,23 +16,22 @@ describe('resolveValue', () => {
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     const errorMsg = 'Something went wrong!';
-    expect(() => throwError(errorMsg)).toThrow(errorMsg);
+    expect(() => throwError(errorMsg)).toThrowError(errorMsg);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    expect(throwError).toThrow('Oops!');
-
+    expect(() => throwError()).toThrowError('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    expect(throwCustomError).toThrow(MyAwesomeError);
+    expect(() => throwCustomError()).toThrowError(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
+    await expect(rejectCustomError()).rejects.toThrowError(MyAwesomeError);
   });
 });
